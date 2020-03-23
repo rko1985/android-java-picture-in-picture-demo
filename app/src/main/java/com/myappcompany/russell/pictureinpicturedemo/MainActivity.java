3e98cb49fb398f79bbce188aece537be46a26a65
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-03-23 11:24:52 -1000

    change text, button and support bar when changing to picture in picture mode

--- app/src/main/java/com/myappcompany/russell/pictureinpicturedemo/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/pictureinpicturedemo/MainActivity.java
@@ -1,8 +1,11 @@
 package com.myappcompany.russell.pictureinpicturedemo;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onPictureInPictureModeChanged(boolean isInPictureInPictureMode, Configuration newConfig) {
+        super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig);
+
+        TextView textView = findViewById(R.id.textView);
+        Button pipButton = findViewById(R.id.pipButton);
+
+        if(isInPictureInPictureMode){
+            //going into PIP
+            pipButton.setVisibility(View.INVISIBLE);
+            getSupportActionBar().hide();
+            textView.setText("$10,043.45");
+        } else {
+            //going out of PIP
+            pipButton.setVisibility(View.VISIBLE);
+            getSupportActionBar().show();
+            textView.setText("Hello World!");
+        }
+
     }
 }
